9305483389514947ee285c2dff91b1d975f03f4b
seonwook <ksu8485@naver.com>
2022-06-13 00:34:18 +0900

    deploy with vuepress script

---
+++ service-worker.js
@@ -0,0 +1,253 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "404.html",
+    "revision": "73021125d86d919b12858504d56e9e52"
+  },
+  {
+    "url": "assets/css/0.styles.b6c17d52.css",
+    "revision": "5cbb788aecb12f05feb81b63a85c6b96"
+  },
+  {
+    "url": "assets/img/search.83621669.svg",
+    "revision": "83621669651b9a3d4bf64d1a670ad856"
+  },
+  {
+    "url": "assets/js/10.7189f1f7.js",
+    "revision": "8578b3289a7e64d609356dda2f406b08"
+  },
+  {
+    "url": "assets/js/2.32449b10.js",
+    "revision": "6244c6f7cbc9ebd5a314668c8effe99b"
+  },
+  {
+    "url": "assets/js/3.00f3699c.js",
+    "revision": "3b295fb7743abce598d1a97998af995f"
+  },
+  {
+    "url": "assets/js/4.2dad5ebd.js",
+    "revision": "1478671928c20c2c6f89ac69b6092e40"
+  },
+  {
+    "url": "assets/js/5.e9aa9570.js",
+    "revision": "4846c220b5c497899d6a76ade7e4ead5"
+  },
+  {
+    "url": "assets/js/6.bff28010.js",
+    "revision": "c6257db418fa1e5c126b4b582e1cc60a"
+  },
+  {
+    "url": "assets/js/7.38790235.js",
+    "revision": "60ea59e9d6c7e7f2d5980f9ed0644a9a"
+  },
+  {
+    "url": "assets/js/8.28f5481d.js",
+    "revision": "38cef8caa38d2f9f05aa765b77f7c176"
+  },
+  {
+    "url": "assets/js/9.89395743.js",
+    "revision": "b85a55349b635f26c20460ecc19cd1cc"
+  },
+  {
+    "url": "assets/js/app.4e4cdcea.js",
+    "revision": "b56295046eaa646d30745ddc1d8d61bc"
+  },
+  {
+    "url": "images/cli-service-inspect-output.png",
+    "revision": "a246557e283d7c5d20491d6ca74b4f41"
+  },
+  {
+    "url": "images/cli-service-webpack.png",
+    "revision": "c626cb7c1b852a8d087a2f20e1d1b3c6"
+  },
+  {
+    "url": "images/component-communication.png",
+    "revision": "2bb1d838870abdeeca7bac6875905292"
+  },
+  {
+    "url": "images/component.png",
+    "revision": "b5c08269dfc26ae6d7db3801e9efd296"
+  },
+  {
+    "url": "images/console-instance.png",
+    "revision": "3d009ae3c98cf33d066a77a7fcee77a5"
+  },
+  {
+    "url": "images/cors-error.png",
+    "revision": "bd772efa871eb9b85ab52eaee8335448"
+  },
+  {
+    "url": "images/cors.png",
+    "revision": "a424b9ad7791b1034e2fd6eccd57c610"
+  },
+  {
+    "url": "images/deploy-folder-structure.png",
+    "revision": "28c17711417d6e8d72613cd9a89fb81a"
+  },
+  {
+    "url": "images/icons/120x.png",
+    "revision": "bdbb30ccb538ba228c8df4eead21e5de"
+  },
+  {
+    "url": "images/icons/128x.png",
+    "revision": "9c3ba34e48ac1acc8eaa5ac68a72c544"
+  },
+  {
+    "url": "images/icons/144x.png",
+    "revision": "202ab576d05bae1b5bef5706c6d16084"
+  },
+  {
+    "url": "images/icons/152x.png",
+    "revision": "c5a0fc796d0059ee5c400cd835aa30a4"
+  },
+  {
+    "url": "images/icons/167x.png",
+    "revision": "1881e234ec550072f06f0cb423e7c9bf"
+  },
+  {
+    "url": "images/icons/180x.png",
+    "revision": "af405def5da96beb1860580c52d99f5c"
+  },
+  {
+    "url": "images/icons/192x.png",
+    "revision": "3aa2b606bccadc8a3463c3ee24c03a96"
+  },
+  {
+    "url": "images/icons/384x.png",
+    "revision": "c8661c6f25676397bd24ea6f99054e19"
+  },
+  {
+    "url": "images/icons/512x.png",
+    "revision": "12a26264e5fb40aab70b51c375a5ab54"
+  },
+  {
+    "url": "images/icons/72x.png",
+    "revision": "692868f0e1e6c63ccd49fe843cb8382f"
+  },
+  {
+    "url": "images/icons/96x.png",
+    "revision": "4f4a131b91c32a1a509bdbd5e27dc636"
+  },
+  {
+    "url": "images/lifecycle.png",
+    "revision": "dcbe29f6cd54d44a5a3a63c6266da681"
+  },
+  {
+    "url": "images/test/cli-option-1.png",
+    "revision": "463df32da6087c1647b07b4a0ff3ef88"
+  },
+  {
+    "url": "images/test/cli-option-2.png",
+    "revision": "04ff38a9269d6889edeb867695510e02"
+  },
+  {
+    "url": "images/test/cli-option-3.png",
+    "revision": "af1be8cd36eec2dc4925cd300dc2e1fb"
+  },
+  {
+    "url": "images/test/cli-option-4.png",
+    "revision": "559572fc3f2e27d44615e7da0f0193b3"
+  },
+  {
+    "url": "images/test/jest-parsing-error.png",
+    "revision": "4473b18cfad9b90d083c1614db0346e5"
+  },
+  {
+    "url": "images/test/test-result.png",
+    "revision": "7a4009b79d6a925c694e93b60ed66a91"
+  },
+  {
+    "url": "images/test/vue-cli-preset-setup.png",
+    "revision": "33042d8b129e01483ae1e4588cc7c738"
+  },
+  {
+    "url": "images/test/vue-cli-test-setup.png",
+    "revision": "41909400db376e7875c67abcea69026c"
+  },
+  {
+    "url": "images/todo-app-todo-control.png",
+    "revision": "75d8a516049fc6ccfdfe691ba3e039c4"
+  },
+  {
+    "url": "images/transition-flow.png",
+    "revision": "5990c1dff7dc7a8fb3b34b4462bd0105"
+  },
+  {
+    "url": "images/ts-error.png",
+    "revision": "c185525a3ac3771189ff0cc3d7c83c68"
+  },
+  {
+    "url": "images/ts-extend-error.png",
+    "revision": "7fd4e033a61565f45685b582775e0c5b"
+  },
+  {
+    "url": "images/ts-presets.png",
+    "revision": "a992ef08644e975aa8ef676aa2495b58"
+  },
+  {
+    "url": "images/vue-component-testing.png",
+    "revision": "9ee4a5127dec97dfa9409c3586338107"
+  },
+  {
+    "url": "images/vuex-concept.png",
+    "revision": "983ea11f68f23d6a3229e13eafea6dc7"
+  },
+  {
+    "url": "images/vuex-flow.png",
+    "revision": "288a0dc913bab3fe765baf18fb4bac27"
+  },
+  {
+    "url": "images/web-dev-flow.png",
+    "revision": "8638e708c620edbad140b9c4b8f050a1"
+  },
+  {
+    "url": "index.html",
+    "revision": "9ca18c518e4a51884304f577455944c9"
+  },
+  {
+    "url": "logo.png",
+    "revision": "8beafafdfcde47c5a072e1c60bf56089"
+  },
+  {
+    "url": "textbook.html",
+    "revision": "39548f52b8837f61172d77d72bbfecc7"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+addEventListener('message', event => {
+  const replyPort = event.ports[0]
+  const message = event.data
+  if (replyPort && message && message.type === 'skip-waiting') {
+    event.waitUntil(
+      self.skipWaiting().then(
+        () => replyPort.postMessage({ error: null }),
+        error => replyPort.postMessage({ error })
+      )
+    )
+  }
+})
